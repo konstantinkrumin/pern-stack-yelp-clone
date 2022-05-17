bc87c636174f7426c24662b4337fce4c2d0b6b18
konstantink1 <konstantink1.dev@gmail.com>
2022-05-16 23:56:53 -0400

    set up rest of routes

--- server/server.js
+++ server/server.js
@@ -7,17 +7,14 @@ const app = express();
 
 app.use(morgan('dev'));
 
-app.use((req, res, next) => {
-	console.log('something');
-	next();
-});
+app.use(express.json());
 
 // Get all restaurants
 app.get('/api/v1/restaurants', (req, res) => {
 	res.status(200).json({
 		status: 'success',
 		data: {
-			restaurants: ['mcdonalds', 'wendys']
+			restaurant: ['mcdonalds', 'wendys']
 		}
 	});
 });
@@ -25,11 +22,43 @@ app.get('/api/v1/restaurants', (req, res) => {
 // Get a restaurant
 app.get('/api/v1/restaurants/:id', (req, res) => {
 	console.log(req.params);
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			restaurant: ['mcdonalds', 'wendys']
+		}
+	});
 });
 
 // Create a restaurant
 app.post('/api/v1/restaurants', (req, res) => {
-	console.log(req);
+	console.log(req.body);
+
+	res.status(201).json({
+		status: 'success',
+		data: {
+			restaurant: ['mcdonalds', 'wendys']
+		}
+	});
+});
+
+app.put('/api/v1/restaurants/:id', (req, res) => {
+	console.log(req.params.id);
+	console.log(req.body);
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			restaurant: ['mcdonalds', 'wendys']
+		}
+	});
+});
+
+app.delete('/api/v1/restaurants/:id', (req, res) => {
+	res.status(204).json({
+		status: 'success'
+	});
 });
 
 const port = process.env.PORT || 4000;
