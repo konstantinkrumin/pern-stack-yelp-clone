25209e2fdb0f4b9517cc8a220492c9f9393ebb06
konstantink1 <konstantink1.dev@gmail.com>
2022-05-16 23:45:02 -0400

    set up routes basic structure and added logging middleware

--- server/server.js
+++ server/server.js
@@ -1,10 +1,39 @@
 require('dotenv').config();
 
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
-const port = process.env.PORT;
+app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+	console.log('something');
+	next();
+});
+
+// Get all restaurants
+app.get('/api/v1/restaurants', (req, res) => {
+	res.status(200).json({
+		status: 'success',
+		data: {
+			restaurants: ['mcdonalds', 'wendys']
+		}
+	});
+});
+
+// Get a restaurant
+app.get('/api/v1/restaurants/:id', (req, res) => {
+	console.log(req.params);
+});
+
+// Create a restaurant
+app.post('/api/v1/restaurants', (req, res) => {
+	console.log(req);
+});
+
+const port = process.env.PORT || 4000;
+
 app.listen(port, () => {
 	console.log(`server is up and listening on port ${port}`);
 });
