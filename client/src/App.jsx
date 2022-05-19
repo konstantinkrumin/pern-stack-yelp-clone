8a302ca8faaa2c621c8814dcc38a53dbe6443a63
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-05-19 02:05:50 -0400

    set up client routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from "./routes/Home";
+import UpdatePage from "./routes/UpdatePage";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/restaurants/:id/update" component={UpdatePage} />
+          <Route
+            exact
+            path="/restaurants/:id"
+            component={RestaurantDetailPage}
+          />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
