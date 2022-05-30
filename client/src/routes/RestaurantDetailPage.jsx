f7607bf0a6762615bc91aee0c4435077288b0fa4
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-05-29 22:20:30 -0400

    set up individual restaurant page

--- client/src/routes/RestaurantDetailPage.jsx
+++ client/src/routes/RestaurantDetailPage.jsx
@@ -16,7 +16,7 @@ const RestaurantDetailPage = () => {
     const fetchData = async () => {
       try {
         const response = await RestaurantFinder.get(`/${id}`);
-        setSelectedRestaurant(response.data.data.restaurant);
+        setSelectedRestaurant(response.data.data);
       } catch (err) {
         console.log(err);
       }
@@ -29,8 +29,11 @@ const RestaurantDetailPage = () => {
     <div>
       {selectedRestaurant && (
         <>
+          <h1 className="text-center display-1">
+            {selectedRestaurant.restaurant.name}
+          </h1>
           <div className="mt-3">
-            <Reviews />
+            <Reviews reviews={selectedRestaurant.reviews} />
             <AddReview />
           </div>
         </>
