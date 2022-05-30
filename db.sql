af99eb5b073c57fc97db5e936cef16d7dcae46cb
Konstantin Krumin <konstantin.krumin@gmail.com>
2022-05-29 20:59:55 -0400

    set up postgres for reviews

--- db.sql
+++ db.sql
@@ -21,6 +21,14 @@ CREATE TABLE restaurants (
     price_range INT NOT NULL check(price_range >= 1 and price_range <= 5)
 );
 
+CREATE TABLE reviews(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
+    name VARCHAR(50) NOT NULL,
+    review TEXT NOT NULL,
+    rating INT NOT NULL check(rating >= 1 and rating <= 5)
+);
+
 -- to list all tables: \d
 
 -- to see the schema of the table: \d [table name]
